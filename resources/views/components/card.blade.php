61a4e1a663bfce31747955995a8601dcf28b4308
felipesantos22 <felipevs2018@gmail.com>
2024-06-29 11:35:20 -0300

    change css of card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,17 +1,21 @@
+<link rel="stylesheet" href="{{ asset('css/card.css') }}" />
+
 <div class="container mt-5">
     <div class="row">
-        @foreach ($houses as $house)
-            <div class="col-md-4 mb-4">
-                <div class="card" style="width: 14rem;">
-                    <img src="{{ url("storage/{$house->photo}") }}" class="card-img-top" alt="{{ $house->type }}">
-                    <div class="card-body">
-                        <p class="card-text">{{ $house->city }}</p>
-                        <h5 class="card-title">{{ $house->type }}</h5>
-                        <p class="card-text"><b>R$ {{ $house->formatted_price }}</b></p>
-                        <a href="{{ route('show', $house->id) }}" class="btn btn-dark">Ver Detalhes</a>
+        <div class="card-containers">
+            @foreach ($houses as $house)
+                <div class="col-md-3 mb-3">
+                    <div class="card" style="width: 14rem;">
+                        <img src="{{ url("storage/{$house->photo}") }}" class="card-img-top" alt="{{ $house->type }}">
+                        <div class="card-body">
+                            <p class="card-text">{{ $house->city }}</p>
+                            <h5 class="card-title">{{ $house->type }}</h5>
+                            <p class="card-text"><b>R$ {{ $house->formatted_price }}</b></p>
+                            <a href="{{ route('show', $house->id) }}" class="btn btn-dark">Ver Detalhes</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </div>
