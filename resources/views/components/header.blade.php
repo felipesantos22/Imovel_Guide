f6b0508b1c2a137f087f740f6fd63ecca5240ab3
felipevs2022 <felipevs2022@gmail.com>
2024-11-18 06:57:51 -0300

    change color header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="p-3" style="background-color: #1b2653; color: white">
+<header class="p-3" style="background-color: #1b2654; color: white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
